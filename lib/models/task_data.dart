e7883d4b497ecaf85c1b3c7137bd97edcf972abe
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2022-08-13 11:53:15 +0200

    first commit

---
+++ lib/models/task_data.dart
@@ -0,0 +1,36 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:todoey/models/task.dart';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  void addNewTask(String taskTitle) {
+    final task = Task(name: taskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+}
---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task.dart';
+import 'package:todoey/models/task_data.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  final Function addTaskCallback;
+  String newTaskTitle;
+
+  AddTaskScreen({this.addTaskCallback});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                newTaskTitle = value;
+              },
+            ),
+            FlatButton(
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                // final task = Task(name: newTaskTitle);
+                Provider.of<TaskData>(context, listen: false)
+                    .addNewTask(newTaskTitle);
+                Navigator.pop(context);
+              },
+              child: Text(
+                'Add',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
